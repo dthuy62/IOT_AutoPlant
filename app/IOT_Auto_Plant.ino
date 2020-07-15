fb617c373114c87bc87f9691bf2d2162883aec43
dthuy62 <thanhhuy6220@gmail.com>
2020-07-15 19:14:18 +0700

    commit

--- app/IOT_Auto_Plant.ino
+++ app/IOT_Auto_Plant.ino
@@ -4,10 +4,10 @@
 //
 //
 // Set these to run example.
-#define FIREBASE_HOST "arduinofirebase-e5475.firebaseio.com"
-#define FIREBASE_AUTH "TfLEywXnlwPpmv0fsWEAUpNrbBXuSiprk6FyMTfk"
-#define WIFI_SSID "a nho"
-#define WIFI_PASSWORD "123456789"
+#define FIREBASE_HOST "autoplant-b5120.firebaseio.com"
+#define FIREBASE_AUTH "vctg4Q2avv08fCFSZHXo306fkonRlfMQaKehi8A4"
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
 
 #define WATER_PUMP 2
 
@@ -42,20 +42,18 @@ void loop() {
    soil_moisture= analogRead(sensor_pin);
     soil_moisture = map(soil_moisture, 500, 1023, 100, 0);
    Serial.println(soil_moisture);
-   Firebase.setInt("Doam",soil_moisture);
+   Firebase.setInt("Do am dat",soil_moisture);
    delay(1000);
    n = Firebase.getInt("Status");
-   m = Firebase.getInt("Doam");
- if (n==1  || m<20) {
+   m = Firebase.getInt("Do am dat");
+    if (n==1  || m<20) {
     Serial.println("PUMP ON");
     digitalWrite(WATER_PUMP,HIGH);  
     return;
     }
-
-      }
     else  {
     Serial.println("PUMP OFF");
     digitalWrite(WATER_PUMP,LOW);  
 }
-
 }
+
